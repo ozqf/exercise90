feba15890cc2764002c33165c58938a0ab992e48
zqf <zqf@zealousquakefan.com>
2018-03-16 14:15:54 +0000

    > Added cmd to init vars for cl.exe for VS 2017 (for future reference) > gitignore now ignores lib files in lib dir

--- shellLaptop.bat
+++ shellLaptop.bat
@@ -1,5 +1,9 @@
 @echo off
 subst y: E:\dropbox\projects\cpp\exercise90
+@rem 2017:
+@rem "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
+@rem before 2017:
+@rem "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 y:
