f0546ccf193b95cb742b7d82c11f030a5c4ee63c
zqf <zqf@zealousquakefan.com>
2018-05-22 06:57:56 +0100

    More text parsing .obj files

--- src/Tests/test_read_mesh.h
+++ src/Tests/test_read_mesh.h
@@ -7,23 +7,148 @@
 #define READ_MODE_NUMBER 1
 #define READ_MODE_SKIP_LINE 2
 
+struct FileReadInfo
+{
+    FILE* f;
+    u32 pos;
+    u32 length;
+};
+
+struct MeshStats
+{
+    Vec3* vertices;
+    u32 numVertices;
+    u32 maxVertices;
+    Vec3* triangles;
+    u32 numTriangles;
+    u32 maxTriangles;
+};
+
+struct ObjFileProperties
+{
+    u32 fileSize;
+    u32 numVertices;
+    u32 numFaces;
+};
+
+void Test_SkipLine(FileReadInfo* f);
+
+ObjFileProperties Test_ReadObjProperties(FileReadInfo* f)
+{
+    ObjFileProperties props = {};
+
+    fseek(f->f, 0, SEEK_END);
+    props.fileSize = ftell(f->f);
+    fseek(f->f, 0, SEEK_SET);
+
+    u32 pos = 0;
+    u8 mode = READ_MODE_NONE;
+
+    for (;;)
+    {
+        char c;
+        fread(&c, 1, 1, f->f);
+        pos++;
+        if (pos >= props.fileSize)
+        {
+            break;
+        }
+        if (mode == READ_MODE_SKIP_LINE)
+        {
+            if (c == '\n')
+            {
+                mode = READ_MODE_NONE;
+                continue;
+            }
+        }
+        else
+        {
+            if (c == 'v')
+            {
+                props.numVertices++;
+                //mode = READ_MODE_SKIP_LINE;
+                Test_SkipLine(f);
+                continue;
+            }
+            else if (c == 'f')
+            {
+                props.numFaces++;
+                Test_SkipLine(f);
+                //mode = READ_MODE_SKIP_LINE;
+                continue;
+            }
+            else if (c == '\n')
+            {
+                // Make sure detecting empty lines doesn't cause excess skipping
+            }
+            else
+            {
+                mode = READ_MODE_SKIP_LINE;
+            }
+        }
+    }
+
+    fseek(f->f, 0, SEEK_SET);
+
+    return props;
+}
+
+void Test_SkipLine(FileReadInfo* f)
+{
+    char c = 0;
+    do
+    {
+        fread(&c, 1, 1, f->f);
+        f->pos++;
+    } while (c != '\n' && f->pos < f->length);
+}
+
+f32 Test_ReadVertex(FileReadInfo* f, MeshStats* stats)
+{
+    char buf[32];
+    i32 bufPos = 0;
+    char c;
+    do
+    {
+        fread(&c, 1, 1, f->f);
+        f->pos++;
+        
+        // Read into parse buffer
+        if ((c >= '0' && c <= '9') || c == '.')
+        {
+            buf[bufPos] = c;
+            bufPos++;
+        }
+        else
+        {
+            break;
+        }
+
+    } while (c != '\n' && f->pos < f->length);
+
+    // end reading the float and parse
+    buf[bufPos] = NULL;
+    bufPos++;
+    return (f32)atof(buf);
+}
+
 void Test_ReadMesh()
 {
     char *path = "./base/box.obj";
-    FILE *f;
-    fopen_s(&f, path, "r");
-    if (f == NULL)
+    
+    FileReadInfo info = {};
+    fopen_s(&info.f, path, "r");
+    if (info.f == NULL)
     {
         printf("Failed to open %s...Aboring\n", path);
         return;
     }
-    i32 end;
-    fseek(f, 0, SEEK_END);
-    end = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    printf("Read %d chars in %s\n", end, path);
+    fseek(info.f, 0, SEEK_END);
+    info.length = ftell(info.f);
+    fseek(info.f, 0, SEEK_SET);
+    printf("Read %d chars in %s\n", info.length, path);
 
-    i32 pos = 0;
+    info.pos = 0;
 
     u8 reading = 1;
     u32 count = 0;
@@ -37,18 +162,25 @@ void Test_ReadMesh()
     i32 numVerts = 0;
     i32 numFaces = 0;
 
+    ObjFileProperties props = Test_ReadObjProperties(&info);
+
+    MeshStats mesh = {};
+    mesh.maxVertices = props.numVertices;
+    mesh.vertices = (Vec3*)malloc(sizeof(Vec3) * mesh.maxVertices);
+
+#if 1
     while (reading)
     {
         char c;
-        fread(&c, 1, 1, f);
+        fread(&c, 1, 1, info.f);
 
-        pos++;
-        if (pos >= end)
+        info.pos++;
+        if (info.pos >= info.length)
         {
             break;
         }
         
-        if (pos > 1000)
+        if (info.pos > 1000)
         {
             break;
         }
@@ -64,6 +196,7 @@ void Test_ReadMesh()
                 }
             }
             break;
+#if 0
             case READ_MODE_NUMBER:
             {
                 if ((c >= '0' && c <= '9') || c == '.')
@@ -83,6 +216,7 @@ void Test_ReadMesh()
                 }
             }
             break;
+#endif
             default:
             {
                 if (c == '#')
@@ -122,8 +256,9 @@ void Test_ReadMesh()
             break;
         }
     }
+#endif
 
-    fclose(f);
+    fclose(info.f);
 
-    printf("Printed %d characters\nRead %d vertices and %d faces\n", count, numVerts, numFaces);
+    printf("Printed %d characters\nRead %d vertices and %d faces\n", count, props.numVertices, props.numFaces);
 }
