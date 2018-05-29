dadbaef45255d3a7416788f14f1e1408dc96cdcb
zqf <zqf@zealousquakefan.com>
2018-05-29 23:54:40 +0100

    Added Save file recursive loading.

--- src/Tests/test_state_file.h
+++ src/Tests/test_state_file.h
@@ -5,7 +5,10 @@
 struct StateSaveHeader
 {
     char magic[4] = { 'S', 'A', 'V', 'E' };
-    u32 numEntities = 0;
+    char baseFile[32];
+    u32 numStaticEntities = 0;
+    u32 numDynamicEntities = 0;
+    u32 numFrames = 0;
 };
 
 struct CmdSpawn
@@ -14,7 +17,7 @@ struct CmdSpawn
     f32 pos[3];
 };
 
-void Test_WriteStateFile(char* fileName)
+void Test_WriteStateFile(char* fileName, char* baseFileName)
 {
     printf("Write savestate\n");
     FILE* f;
@@ -28,6 +31,15 @@ void Test_WriteStateFile(char* fileName)
     fseek(f, sizeof(StateSaveHeader), SEEK_SET);
 
     StateSaveHeader header = {};
+    if (baseFileName != NULL)
+    {
+        COM_CopyStringLimited(baseFileName, header.baseFile, 16);
+    }
+    else
+    {
+        COM_ZeroMemory((u8*)header.baseFile, 32);
+    }
+    
 
     CmdSpawn spawn;
     spawn = {};
@@ -35,7 +47,7 @@ void Test_WriteStateFile(char* fileName)
     spawn.pos[0] = 1;
     spawn.pos[1] = 2;
     spawn.pos[2] = 3;
-    header.numEntities++;
+    header.numDynamicEntities++;
     fwrite(&spawn, sizeof(CmdSpawn), 1, f);
 
     spawn = {};
@@ -43,7 +55,7 @@ void Test_WriteStateFile(char* fileName)
     spawn.pos[0] = 4;
     spawn.pos[1] = 5;
     spawn.pos[2] = 6;
-    header.numEntities++;
+    header.numDynamicEntities++;
     fwrite(&spawn, sizeof(CmdSpawn), 1, f);
 
     spawn = {};
@@ -51,7 +63,7 @@ void Test_WriteStateFile(char* fileName)
     spawn.pos[0] = 7;
     spawn.pos[1] = 8;
     spawn.pos[2] = 9;
-    header.numEntities++;
+    header.numDynamicEntities++;
     fwrite(&spawn, sizeof(CmdSpawn), 1, f);
 
     // step back, write header
@@ -61,24 +73,24 @@ void Test_WriteStateFile(char* fileName)
 
     fseek(f, 0, SEEK_END);
     u32 end = ftell(f);
-    printf("Wrote %d bytes (%d entities) to \"%s\"\n\n", end, header.numEntities, fileName);
+    printf("Wrote %d bytes (%d entities) to \"%s\"\n\n", end, header.numDynamicEntities, fileName);
     fclose(f);
 }
 
-void Test_ReadState(char* fileName)
+u8 Test_ReadState(char* fileName, u8 staticEntitiesOnly)
 {
     FILE* f;
     fopen_s(&f, fileName, "rb");
     if (f == NULL)
     {
         printf("Failed to open file %s for reading, aborted.\n", fileName);
-        return;
+        return 1;
     }
 
     fseek(f, 0, SEEK_END);
     u32 end = ftell(f);
     fseek(f, 0, SEEK_SET);
-    printf("Reading %d bytes from \"%s\"\n", end, fileName);
+    printf("Reading %d bytes from \"%s\". Static only: %d\n", end, fileName, staticEntitiesOnly);
 
     StateSaveHeader h = {};
     fread(&h, sizeof(StateSaveHeader), 1, f);
@@ -91,10 +103,26 @@ void Test_ReadState(char* fileName)
     {
         printf("File \"%s\" is not a SAVE file\n", fileName);
         fclose(f);
-        return;
+        return 2;
     }
-    printf("Loading %d entities\n", h.numEntities);
-    for(u32 i = 0; i < h.numEntities; ++i)
+    printf("Has %d entities\n", h.numDynamicEntities);
+    if (h.baseFile[0] != NULL)
+    {
+        printf("Reading base file \"%s\"\n", h.baseFile);
+        // TODO: Nothing to prevent circular references here
+        if (Test_ReadState(h.baseFile, 1))
+        {
+            printf("Error reading base file. aborting\n");
+            fclose(f);
+            return 3;
+        }
+        printf("Read base file\n");
+    }
+    else
+    {
+        printf("No base file\n");
+    }
+    for(u32 i = 0; i < h.numDynamicEntities; ++i)
     {
         CmdSpawn s = {};
         fread(&s, sizeof(CmdSpawn), 1, f);
@@ -102,10 +130,12 @@ void Test_ReadState(char* fileName)
     }
 
     fclose(f);
+    return 0;
 }
 
 void Test_StateSaving()
 {
-    Test_WriteStateFile("map.lvl");
-    Test_ReadState("map.lvl");
+    Test_WriteStateFile("map1.lvl", NULL);
+    Test_WriteStateFile("map2.lvl", "map1.lvl");
+    Test_ReadState("map2.lvl", 0);
 }
