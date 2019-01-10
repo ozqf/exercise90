a42fc685ed0fd1da68b179673977316fb809d59c
zqf <zqf@zealousquakefan.com>
2019-01-10 00:04:46 +0000

    Adding stream functions interface

---
+++ src/app/stream.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../common/com_module.h"
+#include "commands.h"
+
+internal TransmissionRecord*
+    Stream_AssignTransmissionRecord(
+        TransmissionRecord* records,
+        u32 sequence)
+{
+
+}
+
+internal TransmissionRecord*
+    Stream_FindTransmissionRecord(
+        TransmissionRecord* records,
+        u32 sequence)
+{
+
+}
+internal void Stream_DeleteCommand(ByteBuffer* b Command* cmd)
+{
+
+}
+
+internal void Stream_FindMessageBySequence(ByteBuffer* b, i32 sequence)
+{
+
+}
+
+internal void Stream_ClearReceivedOutput(NetStream* stream, u32 packetSequence)
+{
+
+}
