71d482b3026af042483525a452818e8154d0cb08
zqf <zqf@zealousquakefan.com>
2019-02-22 21:17:58 +0000

    Implemented further functionality for priority queue.

--- src/app/server/server_priority.h
+++ src/app/server/server_priority.h
@@ -1,5 +1,17 @@
 #pragma once
+/*
+Management of the priority queue used to select unreliable updates
 
+Every Tick:
+> Priority is accumulated each frame (link.importance += link.priority)
+> List is sorted in order of importance
+During packet construction:
+> Iterate over the priority queue. Write entities (and reset importance to 0)
+	until no space is left.
+Repeat
+default priority is 1. If a type of object requires more synchronisation
+than others, increase it's priority value so it accumulates importance faster
+*/
 #include "../../common/com_defines.h"
 
 struct SVEntityLink
@@ -10,6 +22,13 @@ struct SVEntityLink
     f32 importance;
 };
 
+struct SVEntityLinkArray
+{
+	SVEntityLink* links;
+	i32 numLinks;
+	i32 maxLinks;
+};
+
 internal void SV_SwapEntityLinks(
     SVEntityLink* a,
     SVEntityLink* b)
@@ -18,6 +37,48 @@ internal void SV_SwapEntityLinks(
     temp = *a;
     *a = *b;
     *b = temp;
+	
+}
+
+internal i32 SV_GetPriorityLinkIndexById(SVEntityLinkArray* list, i32 id)
+{
+	for (i32 i = 0; i < list->numLinks; ++i)
+	{
+		if (list->links[i].id == id)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+internal void SV_AddPriorityLink(SVEntityLinkArray* list, i32 id, f32 priority)
+{
+	// Avoid duplicates
+	if (SV_GetPriorityLinkIndexById(list, id) != -1)
+	{
+		printf("SV priority: id %d already linked!\n", id);
+		return;
+	}
+	
+	i32 i = list->numLinks;
+	Assert(i < list->maxLinks)
+	if (priority < 1)
+	{
+		priority = 1;
+	}
+	list->numLinks++;
+	list->links[i].id = id;
+	list->links[i].priority = priority;
+}
+
+internal void SV_RemovePriorityLink(SVEntityLinkArray* list, i32 index)
+{
+	if (index == -1) { return; }
+	i32 last = list->numLinks - 1;
+	list->links[index] = {};
+	SV_SwapEntityLinks(&list->links[index], &list->links[last]);
+	list->numLinks -= 1;
 }
 
 internal i32 SV_CompareLink(SVEntityLink* a, SVEntityLink* b)
@@ -39,7 +100,7 @@ internal void SV_BubbleSortPriorityQueue(
         {
             SVEntityLink* a = &links[i];
             SVEntityLink* b = &links[i + 1];
-            if (SV_CompareLink(a, b) > 0)
+            if (SV_CompareLink(a, b) < 0)
             {
                 SV_SwapEntityLinks(a, b);
                 swapped = 1;
@@ -61,4 +122,5 @@ internal void SV_TickPriorityQueue(SVEntityLink* links, i32 numLinks)
         if (!link->inUse) { continue; }
         link->importance += link->priority;
     }
+	SV_BubbleSortPriorityQueue(links, numLinks);
 }
--- src/tools/tests/test_priority.h
+++ src/tools/tests/test_priority.h
@@ -3,18 +3,65 @@
 #include "../../common/com_module.h"
 #include "../../app/server/server_priority.h"
 
-internal void ListPriority(SVEntityLink* links, i32 numLinks)
+internal void ListPriority(SVEntityLinkArray* list)
 {
-    printf("Priority\n");
-    for (i32 i = 0; i < numLinks; ++i)
+    printf("Priority list (%d/%d items)\n",
+		list->numLinks, list->maxLinks);
+    for (i32 i = 0; i < list->numLinks; ++i)
     {
-        printf("Link importance: %.3f\n", links[i].importance );
+        printf("%d Link id %d importance: %.3f\n",
+			i, list->links[i].id, list->links[i].importance);
     }
+}
+
+internal void ResetTopPriorityItems(SVEntityLinkArray* list, i32 numToClear)
+{
+	if (numToClear > list->numLinks)
+	{
+		numToClear = list->numLinks;
+	}
+	for (i32 i = 0; i < numToClear; ++i)
+	{
+		list->links[i].importance = 0;
+	}
 }
 
 internal void Test_Priority()
 {
     printf("Test Priority\n");
+	
+	SVEntityLinkArray list = {};
+	i32 maxLinks = 8;
+	list.links = (SVEntityLink*)malloc(sizeof(SVEntityLink) * maxLinks);
+	list.maxLinks = 8;
+	
+	printf("Empty list:\n");
+	
+	SV_AddPriorityLink(&list, 1, 2);
+	SV_AddPriorityLink(&list, 2, 1);
+	SV_AddPriorityLink(&list, 3, 4);
+	SV_AddPriorityLink(&list, 4, 3);
+	SV_AddPriorityLink(&list, 5, 2);
+	SV_AddPriorityLink(&list, 6, 4);
+	SV_AddPriorityLink(&list, 7, 5);
+	SV_AddPriorityLink(&list, 7, 5);
+	ListPriority(&list);
+	for (i32 i = 0; i < 10; ++i)
+	{
+		SV_TickPriorityQueue(list.links, list.numLinks);
+	}
+	SV_RemovePriorityLink(
+		&list,
+		SV_GetPriorityLinkIndexById(&list, 4)
+	);
+	SV_RemovePriorityLink(
+		&list,
+		SV_GetPriorityLinkIndexById(&list, 2)
+	);
+	ResetTopPriorityItems(&list, 3);
+	SV_TickPriorityQueue(list.links, list.numLinks);
+	ListPriority(&list);
+	/*
     const i32 numLinks = 4;
     SVEntityLink links[numLinks];
     links[0] = {};
@@ -29,4 +76,5 @@ internal void Test_Priority()
     ListPriority(links, numLinks);
     SV_BubbleSortPriorityQueue(links, numLinks);
     ListPriority(links, numLinks);
+	*/
 }
