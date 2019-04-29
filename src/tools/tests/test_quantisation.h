e7b326f18f7cafb2062879a04577f7336b739644
zqf <zqf@zealousquakefan.com>
2019-04-30 00:59:11 +0100

    Fixed quantisation tests

--- src/tools/tests/test_quantisation.h
+++ src/tools/tests/test_quantisation.h
@@ -29,7 +29,7 @@ i32 CalcQuantizationBits(i32 highLimit, i32 lowLimit)
 const i32 majorRange = 32;
 const i32 precisionBits = 9;
 
-i16 Test_QuantiseF32ToI16_B(f32 input, const i16 halfRange)
+u16 Test_QuantiseF32ToI16_B(f32 input, const i16 halfRange)
 {
     const i32 totalBits = 16;
     i32 majorBits = (i32)log2f((f32)halfRange * 2);
@@ -39,10 +39,10 @@ i16 Test_QuantiseF32ToI16_B(f32 input, const i16 halfRange)
     f32 scale = powf(2, (f32)minorBits);
     input *= scale;
     printf("  scaled up result: %f\n", input);
-    return (i16)input;
+    return (u16)input;
 }
 
-f32 Test_DequantiseI16ToF32_B(i16 input, const i16 halfRange)
+f32 Test_DequantiseI16ToF32_B(u16 input, const i16 halfRange)
 {
     const i16 totalBits = 16;
     i32 majorBits = (i32)log2f((f32)halfRange * 2);
@@ -82,10 +82,21 @@ void Test_QuantiseValue(f32 original)
 {
     // i16 encoded = Test_QuantiseF32ToI16(original);
     // f32 result = Test_DequantiseI16ToF32(encoded);
-    printf("> %f\n", original);
+    printf("-----\n> %f\n", original);
     i16 encoded = Test_QuantiseF32ToI16_B(original, 32);
     f32 result = Test_DequantiseI16ToF32_B(encoded, 32);
     printf("%f -> %d -> %f\n", original, encoded, result);
+    f32 diff = original - result;
+    if (diff < 0.0f) { diff *= -1; }
+    if (diff < 0.01f)
+    {
+        printf("Okay!\n");
+    }
+    else
+    {
+        printf("Bad\n");
+    }
+    
 }
 
 void Tests_Quantisation()
@@ -111,6 +122,7 @@ void Tests_Quantisation()
     tickIndex, 1 byte
     32 bytes total.
     */
+    #if 0
     printf("\nPosition quantization...\n");
     CalcQuantizationBits(32, 256);
     CalcQuantizationBits(64, 128);
@@ -121,14 +133,18 @@ void Tests_Quantisation()
 
     printf("\nVelocity quantization...\n");
     CalcQuantizationBits(128, 128);
-
+    #endif
     printf("\n-- Quantise Tests --\n");
+    Test_QuantiseValue(24.0f);
+    Test_QuantiseValue(26.0f);
     Test_QuantiseValue(29.35f);
+    Test_QuantiseValue(31.1f);
     Test_QuantiseValue(-15.794f);
-    printf("And breaking it:\n");
-    Test_QuantiseValue(32.0f);
-    Test_QuantiseValue(-32.0f);
-    Test_QuantiseValue(-33.0f);
+    //printf("And breaking it:\n");
+    //Test_QuantiseValue(32.0f);
+    //Test_QuantiseValue(-32.0f);
+    //Test_QuantiseValue(-33.0f);
+
     //f32 testValue = 27.9251f;
     //i32 numBits = (i32)ceilf(log2f(testValue));
     //printf("Num bits for %f: %d\n", testValue, numBits);
