356b4a68786d6041b0a3e7828621ea74ccda8a52
zqf <zqf@zealousquakefan.com>
2019-09-25 23:51:35 +0100

    More blender file parsing

--- src/tools/ze_read_blender_file.h
+++ src/tools/ze_read_blender_file.h
@@ -17,10 +17,11 @@ struct ZEBlenderDataObj
     char asciCode[4];
     u32 size;
     // pointer size could be 4 or 8 bytes
-    u64 ptr;
+    u64 oldPtr;
     u32 SDNAIndex;
     u32 numElements;
     u32 numBytes;
+    void* dataPtr;
 };
 
 struct ZEBlenderFileDef
@@ -37,8 +38,11 @@ struct ZEBlenderFileDef
 // Functions
 //////////////////////////////////////////////////////////
 static i32 ZE_ReadBlenderDataObjHeaders(
-    i32 pointerSize, u8* mem, ZEBlenderDataObj** out_objects, u32* out_numObjects)
+    i32 pointerSize, u8* mem, ZEBlenderDataObj** out_objects, u32* out_numObjects, i32* out_dna1Index)
 {
+    *out_objects = NULL;
+    *out_numObjects = 0;
+    *out_dna1Index = -1;
     // TODO: Make this resizeable... or scan for size before allocating array.
     // Even a simple object seemed to have 5k+ objects...
     const u32 maxObjects = 10000;
@@ -46,14 +50,16 @@ static i32 ZE_ReadBlenderDataObjHeaders(
     printf("Allocating %dKB for file table\n", bytesForTable / 1024);
     ZEBlenderDataObj* objects = (ZEBlenderDataObj*)malloc(bytesForTable);
     u32 numObjects = 0;
+    i32 dna1Index = -1;
     
-    i32 bVerbose = YES;
+    i32 bVerbose = NO;
     i32 numVerbose = 10;
     u8* cursor = mem;
 	
 	// stats
     i32 stat_numHeaders = 0;
 	i32 stat_numDataItems = 0;
+    i32 stat_numSDNA = 0;
     for (;;)
     {
         if (numObjects == maxObjects)
@@ -80,6 +86,14 @@ static i32 ZE_ReadBlenderDataObjHeaders(
 		{
 			stat_numDataItems++;
 		}
+        else if (strncmp(obj->asciCode, "DNA1", 4) == 0)
+		{
+            dna1Index = (numObjects - 1);
+		}
+        else if (strncmp(obj->asciCode, "SDNA", 4) == 0)
+		{
+            stat_numSDNA++;
+		}
         stat_numHeaders++;
         //u32 asciCode = *(u32*)cursor;
         //cursor += sizeof(u32);
@@ -88,12 +102,12 @@ static i32 ZE_ReadBlenderDataObjHeaders(
         
 	    if (pointerSize == 4)
 	    {
-	    	obj->ptr = *(u32*)cursor;
+	    	obj->oldPtr = *(u32*)cursor;
 	    	cursor += 4;
 	    }
 	    else if (pointerSize == 8)
 	    {
-	    	obj->ptr = *(u64*)cursor;
+	    	obj->oldPtr = *(u64*)cursor;
 	    	cursor += 8;
 	    }
 	    else
@@ -102,13 +116,16 @@ static i32 ZE_ReadBlenderDataObjHeaders(
 	    	return 1;
 	    }
 	    if (bVerbose) { printf("Block type \"%c%c%c%c\" - size %d Addr: %lld\n",
-	    	obj->asciCode[0], obj->asciCode[1], obj->asciCode[2], obj->asciCode[3], obj->size, obj->ptr); }
+	    	obj->asciCode[0], obj->asciCode[1], obj->asciCode[2], obj->asciCode[3], obj->size, obj->oldPtr); }
     
 	    obj->SDNAIndex = *(u32*)cursor;
 	    cursor += sizeof(u32);
     
 	    obj->numElements = *(u32*)cursor;
 	    cursor += sizeof(u32);
+
+        // record location of data
+        obj->dataPtr = cursor;
 
 	    if (bVerbose) { printf("SDNA index: %d num elements: %d\n", obj->SDNAIndex, obj->numElements); }
 	    cursor += obj->size;
@@ -117,11 +134,35 @@ static i32 ZE_ReadBlenderDataObjHeaders(
     }
 	//printf("Read %d data objects\n", numHeaders);]
 	printf("Num DATA items: %d\n", stat_numDataItems);
+    printf("Num SDNA items: %d\n", stat_numSDNA);
     *out_objects = objects;
     *out_numObjects = numObjects;
+    *out_dna1Index = dna1Index;
 	return 0;
 }
 
+static i32 ZE_ReadBlenderSDNA(ZEBlenderDataObj* header)
+{
+    printf("Reading SDNA\n");
+    u8* cursor = (u8*)header->dataPtr;
+    const i32 sndaHeaderLength = 8;
+    i32 compResult = strncmp((char*)cursor, "SDNANAME", sndaHeaderLength);
+    if (compResult != 0)
+    {
+        printf("Invalid SNDA header prefix\n");
+        return 1;
+    }
+    cursor += sndaHeaderLength;
+    i32 numBlenderVarNames = *(i32*)cursor;
+    cursor += sizeof(i32);
+    printf("Num blender var names %d\n", numBlenderVarNames);
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////
+// Read file entry point
+////////////////////////////////////////////////////////////
 static i32 ZE_ReadBlenderFile(char* path)
 {
     printf("Reading blender file \"%s\"\n", path);
@@ -201,12 +242,23 @@ static i32 ZE_ReadBlenderFile(char* path)
     def.firstDataObj = (u8*)cursor + 12;
     ZEBlenderDataObj* objList;
     u32 numObjects;
-    if (ZE_ReadBlenderDataObjHeaders(def.pointerSize, def.firstDataObj, &objList, &numObjects) != 0)
+    i32 dna1Index;
+    i32 err = ZE_ReadBlenderDataObjHeaders(
+        def.pointerSize, def.firstDataObj, &objList, &numObjects, &dna1Index);
+    if (err != 0)
     {
         printf("Error reading object headers\n");
         return 1;
     }
-    printf("Read %d object headers\n", numObjects);
+    printf("Read %d object headers. DNA1 index is %d\n", numObjects, dna1Index);
+    if (dna1Index < 0)
+    {
+        printf("Error - Invalid DNA1 index %d\n", dna1Index);
+        free(objList);
+        return 1;
+    }
+
+    err = ZE_ReadBlenderSDNA(&objList[dna1Index]);
 
     // Cleanup
     free(objList);
