fd751eb36bf227467b00c43613702affc9d49a5e
zqf <zqf@zealousquakefan.com>
2020-01-02 00:38:32 +0000

    Checking perlin scatter scale factor

--- src/proc_gen/zpg_perlin_draw.h
+++ src/proc_gen/zpg_perlin_draw.h
@@ -34,15 +34,21 @@ static void ZPG_DrawPerlinGrid(ZPGGrid* grid)
             //u32 pixelIndex = pixelX + (pixelY * grid->width); \
             //ColourU32* pixel = (ColourU32*)&tex->ptrMemory[pixelIndex];
             f32 result;
-            #if 0
+            #if 0 // streaks
             f32 noise = Perlin_Get2d((f32)(x * 5), (f32)(y * 5), 2, 2);
             f32 a = sinf((x + noise / 2) * 50);
             result = (1 + a)  / 2;
             #endif
-            #if 1
+            #if 0 // double sine
             f32 a = (1 + sinf(x * 50 )) / 2;
             f32 b = (1 + sinf(y * 50 )) / 2;
             result = (a * 0.5f) + (b * 0.5f);
+            #endif
+            #if 1 // scatter
+            // Scale factor of 1 means little variation. 10 for a lot
+            f32 scaleFactor = 10;//5;
+            f32 noise = Perlin_Get2d((f32)(x * scaleFactor), (f32)(y * scaleFactor), 2, 2);
+            result = noise > 0.5f ? 1.0f : 0.0f;
             #endif
             // type is scaled to 0-255
             grid->GetCellAt(pixelX, pixelY)->type = (i32)(255.f * result);
